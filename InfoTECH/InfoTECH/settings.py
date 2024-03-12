ad996fce43745043fc736bd22e4ed9f45fc452d9
ved prakash pathak <156571640+Pathak-jii2407@users.noreply.github.com>
2024-03-12 22:19:13 +0530

    Update settings.py

--- InfoTECH/InfoTECH/settings.py
+++ InfoTECH/InfoTECH/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^p&zx#mou7)2o9p_#(vow$1jzidj3#+!nuqfu5(r3ji^ohiy2^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
 
